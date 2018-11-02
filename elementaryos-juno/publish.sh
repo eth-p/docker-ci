49214e65cc9569f34a09e169c0477a626f939483
Ethan P <eth-p+git@hidden.email>
2018-11-02 15:24:47 -0700

    Added build arch to publish.sh

--- elementaryos-juno/publish.sh
+++ elementaryos-juno/publish.sh
@@ -2,3 +2,6 @@
 docker build -t ethp/ci-elementaryos:juno . &&\
 docker push ethp/ci-elementaryos:juno
 
+docker build -t ethp/ci-elementaryos:juno.x86_64 . &&\
+docker push ethp/ci-elementaryos:juno.x86_64
+
